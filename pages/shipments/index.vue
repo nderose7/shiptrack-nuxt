6e4e599cbba54ae78f626823226b72bd075715d9
Nick DeRose <nderose7@gmail.com>
2023-12-29 12:44:54 -0500

    Test index

--- pages/shipments/index.vue
+++ pages/shipments/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full lg:px-10 px-5 pb-32 h-full">
     <div
-      class="w-full mb-5 lg:flex justify-between items-end sticky top-0 pt-5 pb-5 dark:bg-midnight-200 z-50 bg-slate-100"
+      class="w-full mb-5 lg:flex justify-between items-end pt-5 pb-5 dark:bg-midnight-200 z-50 bg-slate-100"
     >
       <div
         class="border-b w-full dark:border-midnight-100 pb-1 flex gap-5 text-center items-center justify-between"
