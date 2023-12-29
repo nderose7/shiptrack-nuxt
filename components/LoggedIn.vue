5433ff63a653e7ff7cd47a770c4d6b3fc59f5388
Nick DeRose <nderose7@gmail.com>
2023-12-29 11:16:52 -0500

    Menu swipe

--- components/LoggedIn.vue
+++ components/LoggedIn.vue
@@ -5,14 +5,17 @@
       :isVisible="sidebarVisible"
       :class="{ visible: sidebarVisible }"
     />
-    <div class="content" :class="{ 'content-expanded': sidebarVisible }">
+    <div
+      class="content"
+      :class="{ 'content-expanded': sidebarVisible }"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
+    >
       <div
         class="swipearea z-50"
         v-if="sidebarVisible"
         @click="sidebarVisible = false"
-        @touchstart="handleTouchStart"
-        @touchmove="handleTouchMove"
-        @touchend="handleTouchEnd"
       ></div>
       <NuxtPage class="z-10" />
     </div>
